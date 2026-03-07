9948c5f6341536b4eff74573020ee62438f5be1b
EmmanuelQuirozA <128554601+EmmanuelQuirozA@users.noreply.github.com>
2026-03-07 17:11:50 -0600

    Revert "Add real-estate diagnosis pages with iframe components and route matching"

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,67 @@ import SolutionsPage from './pages/SolutionsPage'
 import SuccessStoriesPage from './pages/SuccessStoriesPage'
 import ContactPage from './pages/ContactPage'
 import DigitalMaturityQuizPage from './pages/digital-maturity/DigitalMaturityQuizPage'
+import MainLayout from './layouts/MainLayout'
 import DiagnosisPage from './pages/diagnosis/DiagnosisPage'
 import DiagnosisResultPage from './pages/diagnosis/DiagnosisResultPage'
-import { matchDiagnosisRoute } from './routes/routes'
+import { matchDiagnosisRoute, replaceLanguageInPath } from './routes/routes'
+
+const getCurrentPath = () => (typeof window === 'undefined' ? '/' : window.location.pathname)
 
 function App() {
-  const pathname = typeof window === 'undefined' ? '/' : window.location.pathname
+  const [pathname, setPathname] = useState(getCurrentPath)
+
+  useEffect(() => {
+    const handleLocationChange = () => setPathname(window.location.pathname)
+
+    window.addEventListener('popstate', handleLocationChange)
+
+    const nativePushState = window.history.pushState
+    const nativeReplaceState = window.history.replaceState
+
+    window.history.pushState = function pushState(...args) {
+      nativePushState.apply(this, args)
+      handleLocationChange()
+    }
+
+    window.history.replaceState = function replaceState(...args) {
+      nativeReplaceState.apply(this, args)
+      handleLocationChange()
+    }
+
+    return () => {
+      window.removeEventListener('popstate', handleLocationChange)
+      window.history.pushState = nativePushState
+      window.history.replaceState = nativeReplaceState
+    }
+  }, [])
+
   const diagnosisRoute = matchDiagnosisRoute(pathname)
 
   if (diagnosisRoute.type === 'diagnosis') {
-    return <DiagnosisPage language={diagnosisRoute.language} />
+    return (
+      <MainLayout
+        language={diagnosisRoute.language}
+        onLanguageChange={(nextLanguage) => {
+          window.history.pushState({}, '', replaceLanguageInPath(pathname, nextLanguage))
+        }}
+      >
+        <DiagnosisPage language={diagnosisRoute.language} />
+      </MainLayout>
+    )
   }
 
   if (diagnosisRoute.type === 'result') {
-    return <DiagnosisResultPage language={diagnosisRoute.language} tier={diagnosisRoute.tier} />
+    return (
+      <MainLayout
+        language={diagnosisRoute.language}
+        onLanguageChange={(nextLanguage) => {
+          window.history.pushState({}, '', replaceLanguageInPath(pathname, nextLanguage))
+        }}
+      >
+        <DiagnosisResultPage language={diagnosisRoute.language} tier={diagnosisRoute.tier} />
+      </MainLayout>
+    )
   }
 
   if (pathname === '/solutions') {
