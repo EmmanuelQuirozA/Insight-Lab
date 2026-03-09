4dc013bc437fcb17b45f8d99a726f99963be7156
EmmanuelQuirozA <eqa98@outlook.com>
2026-03-08 23:23:48 -0600

    Use of prod API

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export const API_BASE_URL =
-  (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_BASE_URL) ||
-  'https://api.insightlab.com.mx/api'
+export const API_BASE_URL = 'https://api.insightlab.com.mx/api'
